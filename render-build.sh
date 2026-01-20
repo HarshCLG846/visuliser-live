87ed87a64fb0549213c6b42cbc2b642ee38b6abc
HarshCLG846 <23002171210202@mail.ljku.edu.in>
2026-01-20 21:26:07 +0530

    Update backend and frontend

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,11 @@ cd frontend
 npm install
 npm run build
 
+echo "Moving Frontend build to Backend..."
+rm -rf ../backend/static
+mkdir -p ../backend/static
+cp -r dist/* ../backend/static/
+
 echo "Installing Backend Dependencies..."
 cd ..
 pip install -r requirements.txt
